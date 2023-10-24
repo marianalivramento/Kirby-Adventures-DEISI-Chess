8066b24685789713d7006a931799a0cb4539a930
Susana <susana2004wang@outlook.pt>
2023-10-24 21:53:23 +0100

    adicionei ao loadfiles um bocado da lógica que julgo que vamos precisar e comentei a razão que fiz o que fiz

--- src/pt/ulusofona/lp2/deisichess/GameManager.java
+++ src/pt/ulusofona/lp2/deisichess/GameManager.java
@@ -1,6 +1,9 @@
 package pt.ulusofona.lp2.deisichess;
 import javax.swing.*;
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
 
 
@@ -13,6 +16,45 @@ public class GameManager {
 
 
     public boolean loadGame(File file) {
+
+        // tou a aplicar a lógica do projeto passado
+        try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
+            String line;
+            int linha = 0; //para contar o numero de linhas a ajudar a colocar os atributos
+
+            while ((line = reader.readLine()) != null) {
+                String[] tokens = line.split(":");
+
+                if (linha == 0){
+                    // assinalar o tamanho
+                }else if (linha == 1){
+                    // assinalar o numero de peças
+                }else {
+                    /*
+                   if(linhas <= 1+numeroDePecas){
+                        tokens[0] = idDaPeca;
+                        tokens[1] = tipoDaPeca;
+                        tokens[2] = equipa;
+                        tokens[3] = alcunhaDaPeca;
+
+                       -> assinalar cada numero separado por : à sua caracteristica de acordo com o regulamento do projeto
+                    }else{
+                    tokens[0] = quadrado1;
+                        tokens[1] = quadrado2;
+                        tokens[2] = quadrado3;
+                        tokens[3] = quadrado4;
+
+                        -> aqui vemos se os quadrados estão ocupados e obtemos as coordenadas sendo quadrado(n) e n sendo a ordenada=y e (linhas - (1+numeroDePecas)) a abcissa=x
+                    }
+
+                     */
+                }
+
+                linha++;
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         return false;
     }
 
