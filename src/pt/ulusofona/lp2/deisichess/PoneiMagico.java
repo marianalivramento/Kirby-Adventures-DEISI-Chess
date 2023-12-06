d88f515a02b6fa53da20a0cdc5b0dc24b441021b
Mariana <mariana.livramento0@gmail.com>
2023-12-06 09:14:25 +0000

    Erro: PoneiMagico

--- src/pt/ulusofona/lp2/deisichess/PoneiMagico.java
+++ src/pt/ulusofona/lp2/deisichess/PoneiMagico.java
@@ -9,33 +9,38 @@ public class PoneiMagico extends Peca {
 
 
     boolean passaPorPeca(int x0, int y0, int x1, int y1, Jogo jogo) {
-        for (Square s : jogo.getTabuleiro().getQuadrados()) {
-            if (s.getCoordenadaX() != x1 && s.getCoordenadaY() != y1) {
-                if (((x1 == x0 + 2) && (y1 == y0 + 2)) && ((s.getCoordenadaX() == x0 + 1) && ((s.getCoordenadaY() == y0 + 1)))) {
-                    if (s.isOcupado()) {
-                        return true;
-                    }
-                } else if (((x1 == x0 - 2) && (y1 == y0 - 2)) && ((s.getCoordenadaX() == x0 - 1) && ((s.getCoordenadaY() == y0 - 1)))) {
-                    if (s.isOcupado()) {
-                        return true;
-                    }
-                } else if (((x1 == x0 - 2) && (y1 == y0 + 2)) && ((s.getCoordenadaX() == x0 - 1) && ((s.getCoordenadaY() == y0 + 1)))) {
-                    if (s.isOcupado()) {
-                        return true;
-                    }
-                } else if (((x1 == x0 + 2) && (y1 == y0 - 2)) && ((s.getCoordenadaX() == x0 + 1) && ((s.getCoordenadaY() == y0 - 1)))) {
-                    if (s.isOcupado()) {
-                        return true;
+
+            for (int coordenadaY = -2 ; coordenadaY <= 2; coordenadaY++) {
+                Square s = jogo.tabuleiro.retornoQuadrado(x1,coordenadaY);
+                if (coordenadaY != y0 && s != null) {
+                    if (((x1 == x0 + 2) && (y1 == y0 + 2)) && ((s.getCoordenadaX() == x0) && ((s.getCoordenadaY() == y0 + coordenadaY)))) {
+                        if (s.isOcupado()) {
+                            return true;
+                        }
                     }
                 }
             }
-        }
+
+            for (int coordenadaX = -1 ; coordenadaX <= 1; coordenadaX++) {
+                Square s = jogo.tabuleiro.retornoQuadrado(coordenadaX,y1);
+                if (coordenadaX != x0 && s != null) {
+                    if (((x1 == x0 + 2) && (y1 == y0 + 2)) && ((s.getCoordenadaX() == x0 + coordenadaX) && ((s.getCoordenadaY() == y0)))) {
+                        if (s.isOcupado()) {
+                            return true;
+                        }
+                    }
+                }
+            }
 
         return false;
     }
 
     @Override
     boolean movesPermitidos(int x0, int y0, int x1, int y1, Jogo jogo) {
+        if (passaPorPeca(x0,y0,x1,y1,jogo)) {
+            return false;
+        }
+
         if (!pertenceAequipa(jogo, x1, y1)) {
             if ((x1 == x0 + 2) && ((y1 == y0 + 2) || (y1 == y0 - 2))) {
                 return true;
@@ -61,69 +66,31 @@ public class PoneiMagico extends Peca {
 
         if (movesPermitidos(x0, y0, x1, y1, jogo)) {
             if (quadradoDestino.isOcupado()) {
-                if (!passaPorPeca(x0, y0, x1, y1, jogo)) {
+                quadradoDestino.getPeca().setNaoCapturado(false);
+                quadradoDestino.getPeca().setCoordenadas(null);
+                pecaQueMove.setCoordenadas(quadradoDestino);
+                quadradoDestino.setPeca(pecaQueMove);
 
-                    quadradoDestino.getPeca().setNaoCapturado(false);
-                    quadradoDestino.getPeca().setCoordenadas(null);
-                    pecaQueMove.setCoordenadas(quadradoDestino);
-                    quadradoDestino.setPeca(pecaQueMove);
-
-                    quadradoDestino.setOcupado(true);
-                    //equipaPeca.aumentarPecasCapturadas();
-                    jogo.getClassEquipaAtual().aumentarPecasCapturadas();
+                quadradoDestino.setOcupado(true);
+                //equipaPeca.aumentarPecasCapturadas();
+                jogo.getClassEquipaAtual().aumentarPecasCapturadas();
 
 
-                    quadradoOrigem.resetQuadrado();
-                    return true;
-                }
+                quadradoOrigem.resetQuadrado();
+                return true;
 
             } else {
-                if (!passaPorPeca(x0, y0, x1, y1, jogo)) {
-
-                    pecaQueMove.setCoordenadas(quadradoDestino);
-                    quadradoDestino.setPeca(pecaQueMove);
-
-                    quadradoDestino.setOcupado(true);
-                    quadradoOrigem.resetQuadrado();
-                    return true;
-
-                }
-            }
-        }
-/*
-        if (quadradoDestino.isOcupado()) {
-            if (!passaPorPeca(x0, y0, x1, y1, jogo)) {
-                if (!pertenceAequipa(jogo, x1, y1)) {
-                    pecaQueMove.setCoordenadas(quadradoDestino);
-                    quadradoDestino.setPeca(pecaQueMove);
-
-                    quadradoDestino.setOcupado(true);
-                    equipaPeca.aumentarPecasCapturadas();
-
-                    quadradoOrigem.resetQuadrado();
-                    //equipaPeca.aumentarJogadasValidas();
-                    //equipaPeca.numeroDoTurno++;
-                    return true;
-                }
-            }
-        } else {
-            if (!passaPorPeca(x0, y0, x1, y1, jogo)) {
 
                 pecaQueMove.setCoordenadas(quadradoDestino);
                 quadradoDestino.setPeca(pecaQueMove);
-                quadradoOrigem.resetQuadrado();
 
-                //equipaPeca.aumentarJogadasValidas();
-                //equipaPeca.numeroDoTurno++;
+                 quadradoDestino.setOcupado(true);
+                quadradoOrigem.resetQuadrado();
                 return true;
 
             }
         }
 
- */
-
-        //equipaPeca.numeroDoTurno++;
-        //equipaPeca.aumentarTenativasInvalidas();
         return false;
     }
 
