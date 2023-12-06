d422ec32c29b90cfb322da1c4435c0aaf2250f97
Mariana <mariana.livramento0@gmail.com>
2023-12-06 09:16:08 +0000

    Erro: PoneiMagico

--- src/pt/ulusofona/lp2/deisichess/PoneiMagico.java
+++ src/pt/ulusofona/lp2/deisichess/PoneiMagico.java
@@ -9,35 +9,34 @@ public class PoneiMagico extends Peca {
 
 
     boolean passaPorPeca(int x0, int y0, int x1, int y1, Jogo jogo) {
-
-            for (int coordenadaY = -2 ; coordenadaY <= 2; coordenadaY++) {
-                Square s = jogo.tabuleiro.retornoQuadrado(x1,coordenadaY);
-                if (coordenadaY != y0 && s != null) {
-                    if (((x1 == x0 + 2) && (y1 == y0 + 2)) && ((s.getCoordenadaX() == x0) && ((s.getCoordenadaY() == y0 + coordenadaY)))) {
-                        if (s.isOcupado()) {
-                            return true;
-                        }
+        for (int coordenadaY = -2; coordenadaY <= 2; coordenadaY++) {
+            Square s = jogo.tabuleiro.retornoQuadrado(x1, coordenadaY);
+            if (coordenadaY != y0 && s != null) {
+                if (((x1 == x0 + 2) && (y1 == y0 + 2)) && ((s.getCoordenadaX() == x0) && ((s.getCoordenadaY() == y0 + coordenadaY)))) {
+                    if (s.isOcupado()) {
+                        return true;
                     }
                 }
             }
+        }
 
-            for (int coordenadaX = -1 ; coordenadaX <= 1; coordenadaX++) {
-                Square s = jogo.tabuleiro.retornoQuadrado(coordenadaX,y1);
-                if (coordenadaX != x0 && s != null) {
-                    if (((x1 == x0 + 2) && (y1 == y0 + 2)) && ((s.getCoordenadaX() == x0 + coordenadaX) && ((s.getCoordenadaY() == y0)))) {
-                        if (s.isOcupado()) {
-                            return true;
-                        }
+        for (int coordenadaX = -1; coordenadaX <= 1; coordenadaX++) {
+            Square s = jogo.tabuleiro.retornoQuadrado(coordenadaX, y1);
+            if (coordenadaX != x0 && s != null) {
+                if (((x1 == x0 + 2) && (y1 == y0 + 2)) && ((s.getCoordenadaX() == x0 + coordenadaX) && ((s.getCoordenadaY() == y0)))) {
+                    if (s.isOcupado()) {
+                        return true;
                     }
                 }
             }
+        }
 
         return false;
     }
 
     @Override
     boolean movesPermitidos(int x0, int y0, int x1, int y1, Jogo jogo) {
-        if (passaPorPeca(x0,y0,x1,y1,jogo)) {
+        if (passaPorPeca(x0, y0, x1, y1, jogo)) {
             return false;
         }
 
@@ -84,7 +83,7 @@ public class PoneiMagico extends Peca {
                 pecaQueMove.setCoordenadas(quadradoDestino);
                 quadradoDestino.setPeca(pecaQueMove);
 
-                 quadradoDestino.setOcupado(true);
+                quadradoDestino.setOcupado(true);
                 quadradoOrigem.resetQuadrado();
                 return true;
 
