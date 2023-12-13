34ed4dac8a533b17d037d2d175f8613c9575bba2
Susana Wang <susana2004wang@outlook.pt>
2023-12-13 10:00:44 +0000

    espacos bem feitos get hints

--- src/pt/ulusofona/lp2/deisichess/getHints.java
+++ src/pt/ulusofona/lp2/deisichess/getHints.java
@@ -66,9 +66,9 @@ public class getHints implements Comparable<getHints> {
     @Override
     public String toString() {
         if (square.getPeca() != null) {
-            return "(" + square.getCoordenadaX() + ", " + square.getCoordenadaY() + ")->" + square.getPeca().getValor();
+            return "(" + square.getCoordenadaX() + "," + square.getCoordenadaY() + ") -> " + square.getPeca().getValor();
         }
 
-        return "(" + square.getCoordenadaX() + ", " + square.getCoordenadaY() + ")->0";
+        return "(" + square.getCoordenadaX() + "," + square.getCoordenadaY() + ") -> 0";
     }
 }
