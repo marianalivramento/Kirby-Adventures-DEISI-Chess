a176cdd7f86038416fd257c81903856b27a5b6b0
Mariana <mariana.livramento0@gmail.com>
2023-12-29 10:40:18 +0000

    kirby

--- src/pt/ulusofona/lp2/deisichess/Kirby.java
+++ src/pt/ulusofona/lp2/deisichess/Kirby.java
@@ -1,20 +1,5 @@
 package pt.ulusofona.lp2.deisichess;
 
-<<<<<<< HEAD
-public class Kirby extends Peca{
-    int valor = 10;
-
-    public int getValor() {
-        return valor;
-    }
-
-    boolean passaPorPeca(int x0, int y0, int x1, int y1, Jogo jogo) {
-        return false;
-    }
-
-    boolean movesPermitidos(int x0, int y0, int x1, int y1, Jogo jogo) {
-        return true;
-=======
 public class Kirby extends Peca {
 
     private Peca pecaComida;
@@ -28,7 +13,6 @@ public class Kirby extends Peca {
     @Override
     boolean movesPermitidos(int x0, int y0, int x1, int y1, Jogo jogo) {
         return pecaComida.movesPermitidos(x0, y0, x1, y1, jogo);
->>>>>>> origin/main
     }
 
     boolean move(int x0, int y0, int x1, int y1, Jogo jogo) {
@@ -40,10 +24,7 @@ public class Kirby extends Peca {
             if (quadradoDestino.isOcupado()) {
 
                 quadradoDestino.getPeca().setNaoCapturado(false);
-<<<<<<< HEAD
-=======
                 pecaComida = quadradoDestino.getPeca();
->>>>>>> origin/main
                 quadradoDestino.getPeca().setCoordenadas(null);
                 pecaQueMove.setCoordenadas(quadradoDestino);
                 quadradoDestino.setPeca(pecaQueMove);
@@ -54,18 +35,6 @@ public class Kirby extends Peca {
                 quadradoOrigem.resetQuadrado();
                 return true;
 
-<<<<<<< HEAD
-
-            } else {
-                pecaQueMove.setCoordenadas(quadradoDestino);
-                quadradoDestino.setPeca(pecaQueMove);
-
-                quadradoDestino.setOcupado(true);
-                quadradoOrigem.resetQuadrado();
-                return true;
-            }
-
-=======
             } else {
                 pecaQueMove.setCoordenadas(quadradoDestino);
                 quadradoDestino.setPeca(pecaQueMove);
@@ -74,20 +43,12 @@ public class Kirby extends Peca {
 
                 return true;
             }
->>>>>>> origin/main
         }
         return false;
     }
 
-<<<<<<< HEAD
-    public String nomeDoTipo(Jogo jogo) {
-        return "Kirby";
-    }
-
-=======
     @Override
     String nomeDoTipo(Jogo jogo) {
         return "Kirby/" + pecaComida;
     }
->>>>>>> origin/main
 }
